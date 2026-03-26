c4c268a0d86d3c83a26b814b0a20971fd1d02bb6
ngocthanhpham64 <ngocthanhpham64@gmail.com>
2026-03-26 14:43:34 +0700

    first commit

---
+++ auction-system/auction-server/src/main/java/vn/edu/vnu/uet/group8/App.java
@@ -0,0 +1,13 @@
+package vn.edu.vnu.uet.group8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
